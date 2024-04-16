9d63a9db897eacecc7ea3e0bfbb562f7ddd5fa08
dawsbot <3408480+dawsbot@users.noreply.github.com>
2024-04-16 03:23:09 +0000

    Deploying to main from @ dawsbot/dawsbot@0cf0ee43b9aa2bcb7c5475373ea7668eae4f147e 🚀

---
+++ update-degen-sponsors.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+import replaceSection from "replace-section";
+import { generateHtml } from "./degen-sponsors/generate-html";
+import { z } from "zod";
+
+async function main() {
+  const FARCASTER_USERNAME = z.string().parse(process.env.FARCASTER_USERNAME);
+  const DUNE_API_KEY = z.string().parse(process.env.DUNE_API_KEY);
+  const code = await generateHtml(FARCASTER_USERNAME, DUNE_API_KEY);
+
+  const readme = await fs.promises.readFile("README.md", "utf8");
+  const startWith = "<!-- replace-degen-sponsors -->";
+  const endWith = startWith;
+  await fs.promises.writeFile(
+    "README.md",
+    replaceSection({
+      input: readme,
+      startWith,
+      endWith,
+      replaceWith: `${startWith}
+${code}
+${endWith}`,
+    }),
+    "utf-8"
+  );
+}
+
+main();
